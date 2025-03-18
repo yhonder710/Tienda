602a49ff229b7b6b3e165b2aae8c40b824543549
yhonder710 <agueropibe1@gmail.com>
2025-03-18 10:30:59 -0400

    archivos borrados

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="bg-indigo-500 h-80 w-auto text-[18px] mt-10 text-white px-20 py-5 flex justify-between">
-      <section>Conocenos</section>
-      <section>Redes sociales</section>
-      <section>Ayuda</section>
-    </footer>
-  )
-}
