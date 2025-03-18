56e8789ab837f205509ecfd2be87eb9d6d6d4632
yhonder710 <agueropibe1@gmail.com>
2025-03-18 10:45:16 -0400

    base de datos conectada

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./page/Home.jsx"
 import About from "./page/About.jsx"
 import { Naybar } from "./components/Naybar.jsx"
+import { supabase } from "./supabase/supabase.config.jsx"
+import { useEffect } from "react"
 
 
 function App() {
+
+  useEffect(() => {
+    async function datos () {
+      const { data } = await supabase
+      .from('users')
+      .select(`*`)
+      console.log(data)
+    }
+
+    datos()
+  },[])
 
 
   return (
