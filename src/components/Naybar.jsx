07ed414de1992f659073180cd444c3471eab0aff
kicknash01 <olivercordero0000@gmail.com>
2025-03-19 11:12:22 -0400

    colores y bordes

--- src/components/Naybar.jsx
+++ src/components/Naybar.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom"
 
 export function Naybar () {
   return (
-    <div className="flex gap-5">
-      <Link to={'/'}>Home</Link>
-      <Link to={'/login'}>Login</Link>
-      <Link to={'/envios'}>Envios</Link>
-      <Link to={'/products'}>Products</Link>
-      <Link to={'/promociones'}>Promociones</Link>
-    </div>
+      <div className="flex justify-between items-center text-3xl font-bold bg-transparent p-5">
+         <div>
+          <Link className="border-b-3 rounded-4xl p-1.5" to={'/'}>Home</Link>
+         </div>
+         <div className="flex gap-10 border-3 rounded-4xl p-3 justify-items-center">
+          <Link to={'/envios'}>Envios</Link>
+          <Link to={'/products'}>Products</Link>
+          <Link to={'/promociones'}>Promociones</Link>
+         </div>
+         <div>
+          <Link className="border-b-3 rounded-4xl p-2" to={'/login'}>Login</Link>
+         </div>
+      </div>
   )
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1>Home component</h1>
+    <h1 className="text-5xl">Hola</h1>
   )
 }
 
