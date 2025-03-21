f6ae5f2b4a4262c77ae214aefd8f1b21b17ebb0c
kicknash01 <olivercordero0000@gmail.com>
2025-03-21 15:32:08 -0400

    color login mas icono

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -5,9 +5,18 @@ const Login = () => {
   const { signInWithGoogle, signOut, user } = UserAuth()
 
   return (
-    <div className='grid place-content-center min-h-screen'>
-        <h1 className="text-6xl font-bold text-center">LOGIN</h1>
-        <ButtonLogin click={signInWithGoogle}/>
+    <div className='bg-indigo-900'>
+     <div className="grid place-content-center min-h-screen">
+       <section>
+         <img className='w-50 rounded-4xl' src="https://static.vecteezy.com/system/resources/previews/005/076/592/non_2x/hacker-mascot-for-sports-and-esports-logo-free-vector.jpg" alt="" />
+       </section>
+       <section>
+         <h1 className="text-6xl font-bold text-center">LOGIN</h1>
+       </section>
+       <section>
+         <ButtonLogin click={signInWithGoogle}/>
+       </section>
+     </div>
     </div>
   )
 }
