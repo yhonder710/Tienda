bf8bbc14243d3e65e3b6a2c9615954d0da89aea3
yhonder710 <agueropibe1@gmail.com>
2025-03-25 09:32:21 -0400

    cambios minimos

--- src/components/Naybar.jsx
+++ src/components/Naybar.jsx
@@ -1,7 +1,10 @@
 import { Link, useLocation } from "react-router-dom"
 import { UserAuth } from "../context/AuthContext.jsx"
-import { useState } from "react"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
+import { MdExitToApp } from "react-icons/md";
+import { MdLaptop } from "react-icons/md";
+
+
 
 
 
@@ -25,15 +28,15 @@ export function Naybar () {
   return (
       <div className={classNav}>
          <div>
-          <Link className="border-b-3 rounded-4xl p-1.5" to={'/'}>Home</Link>
+          <Link className="flex items-center" to={'/'}>Home</Link>
          </div>
-         <div className="flex gap-10 border-3 rounded-4xl p-3 justify-items-center">
+         <div className="flex gap-5">
           <Link to={'/envios'}>Envios</Link>
           <Link to={'/products'}>Products</Link>
           <Link to={'/promociones'}>Promociones</Link>
          </div>
          <div>
-         <button onClick={signOut} className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2 cursor-pointer w-auto"> Sign Out</button>
+         <button onClick={signOut} className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2 cursor-pointer w-auto gap-2">Sign Out <MdExitToApp size={25}/></button>
          </div>
       </div>
   )
