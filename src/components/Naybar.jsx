8a1a63c1573cfc56adb8b62f6b061e7adc71872c
kicknash01 <olivercordero0000@gmail.com>
2025-03-27 18:35:21 -0400

    cambio en naibar colores

--- src/components/Naybar.jsx
+++ src/components/Naybar.jsx
@@ -35,27 +35,28 @@ export function Naybar () {
     /*  nav */
       <nav className={classNav}>
         {/* section 1 */}
-         <ul className="flex w-full justify-between items-center py-2.5 px-4 text-[18px] bg-indigo-600">
+         <ul className="barra-arriba">
           <li>
             <Link className="home" to={'/'}><MdLaptop size={25}/> Home</Link>
           </li>
           <li className="flex gap-5">
-            <Link className="border-color" to={'/envios'}><FaTruck/> Envios</Link>
-            <Link className="border-color" to={'/products'}><BsBagDashFill/> Products</Link>
-            <Link className="border-color" to={'/promociones'}><BsCurrencyDollar/> Promociones</Link>
+            <Link className="border-color" to={'/envios'}><FaTruck size={25}/> Envios</Link>
+            <Link className="border-color" to={'/products'}><BsBagDashFill size={25}/> Products</Link>
+            <Link className="border-color" to={'/promociones'}><BsCurrencyDollar size={25}/> Promociones</Link>
           </li>
           <li>
           <button type="button" onClick={signOut} className="button-salir">Sign Out <MdExitToApp size={25}/></button>
           </li>
          </ul>
           {/* section 2 */}
-         <section className="nam flex w-full justify-between items-center py-2 px-5 text-[18px] bg-[#24292F]">
+         <section className="nam">
           <article className="flex gap-4 items-center">
             <img src={user.avatar_url} className="h-10 w-auto rounded-full" alt="" />
             <strong>{user.name}</strong>
           </article>
+          {/* carrito */}
           <div className=" flex flex-col justify-center items-center">
-            <button onClick={handleCart} type="button" className="cursor-pointer pr-3">
+            <button onClick={handleCart} type="button" className="carrito cursor-pointer pr-3">
               <FaShoppingCart size={25}/>
             </button>
             <div className={classCart}>
