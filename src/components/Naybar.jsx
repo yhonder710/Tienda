f2ecb8b0f762c918c41c72602e7fea4efd17a898
yhonder710 <agueropibe1@gmail.com>
2025-03-27 14:36:29 -0400

    cart echo

--- src/components/Naybar.jsx
+++ src/components/Naybar.jsx
@@ -14,6 +14,7 @@ export function Naybar () {
   const { signOut, user } = UserAuth()
   const location = useLocation()
   const [nav, setNav] = useState(true)
+  const [cart, setCart] = useState(false)
 
 
   useEffect(() => {
@@ -22,8 +23,13 @@ export function Naybar () {
   }
  },[location])
 
- const classNav = nav ? 'nav on' : 'nav'
+  const classNav = nav ? 'nav on' : 'nav'
+  const classCart = cart ? 'cart on' : 'cart'
+  const classContentCart = cart ? 'contentCard on' : 'contentCard'
 
+ const handleCart = () => {
+  setCart(!cart)
+ }
 
   return (
     /*  nav */
@@ -38,17 +44,24 @@ export function Naybar () {
             <Link className="border-color flex items-center gap-1.5" to={'/products'}><BsBagDashFill/> Products</Link>
             <Link className="border-color flex items-center 0.5" to={'/promociones'}><BsCurrencyDollar/> Promociones</Link>
           </div>
-          <button onClick={signOut} className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#000000]  cursor-pointer w-auto gap-2">Sign Out <MdExitToApp size={25}/></button>
+          <button type="button" onClick={signOut} className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#000000]  cursor-pointer w-auto gap-2">Sign Out <MdExitToApp size={25}/></button>
          </div>
           {/* section 2 */}
-         <div className="nam flex w-full justify-between items-center py-2 px-4 text-[18px] bg-[#24292F]">
-          <div className="flex gap-4 items-center">
-            <img src={user.avatar_url} className="h-10 rounded-full" alt="" />
+         <div className="nam flex w-full justify-between items-center py-2 px-5 text-[18px] bg-[#24292F]">
+          <section className="flex gap-4 items-center">
+            <img src={user.avatar_url} className="h-10 w-auto rounded-full" alt="" />
             <strong>{user.name}</strong>
-          </div>
-          <div>
-            <FaShoppingCart/>
-          </div>
+          </section>
+          <section className=" flex flex-col justify-center items-center">
+            <button onClick={handleCart} type="button" className="cursor-pointer pr-3">
+              <FaShoppingCart size={25}/>
+            </button>
+            <div className={classCart}>
+              <article className={classContentCart}>
+                <h2>hola mundo</h2>
+              </article>
+            </div>
+          </section>
          </div>
       </nav>
   )
